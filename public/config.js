8c1340a77fc199167e897fd3359690a75a249554
Mario Reder <mario.reder@pm.me>
2025-11-05 10:39:48 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Markets,Swap,Portfolio,Rewards,Leaderboard,Vaults",
-  "VITE_CUSTOM_MENUS": "",
+  "VITE_CUSTOM_MENUS": "News, https://sixdex.org/tradingview",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/+EcyrWh8NNlM2ZmM9",
   "VITE_DISCORD_URL": "https://discord.gg/ZE6t3mCddd",
